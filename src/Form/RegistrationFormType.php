e3468d78d4ab63996997bf3d77925140213ec4a5
Andrey Zakhvatoshin <mastetandron@gmail.com>
2025-11-23 20:16:00 +0300

    fix: fix constraints

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -24,15 +24,8 @@ class RegistrationFormType extends AbstractType
                 'mapped' => false,
                 'attr' => ['autocomplete' => 'new-password'],
                 'constraints' => [
-                    new NotBlank([
-                        'message' => 'Please enter a password',
-                    ]),
-                    new Length([
-                        'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
-                        'max' => 4096,
-                    ]),
+                    new NotBlank(message: 'Please enter a password'),
+                    new Length(min: 6, max: 4096, minMessage: 'Your password must be at least 6 characters long'),
                 ],
             ])
         ;
